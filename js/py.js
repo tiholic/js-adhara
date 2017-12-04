794b5e3540f3a73cb3f634523779fc49fada297d
Rohit Reddy Abbadi <rr.16566@gmail.com>
2017-12-04 11:44:43 +0530

    Write code in a way similar to python... py.js contains utilities for the same.

---
+++ js/py.js
@@ -0,0 +1,23 @@
+class RangeIterator {
+    constructor(start, stop, step=1) {
+        this.value = start;
+        this.stop = stop;
+        this.step = step;
+    }
+
+    [Symbol.iterator]() { return this; }
+
+    next() {
+        let value = this.value;
+        if (value + this.step <= this.stop) {
+            this.value += this.step;
+            return {done: false, value: value};
+        } else {
+            return {done: true, value: undefined};
+        }
+    }
+}
+
+function range(start, stop, step) {
+    return new RangeIterator(start, stop, step);
+}
