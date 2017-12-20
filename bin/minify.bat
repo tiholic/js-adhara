c24d455119af2fa0b290cf78f0687850181691cf
Varun2604 <iyervarun2604@gmail.com>
2017-12-21 03:22:08 +0530

    refactoring, and minor changes

--- bin/minify.bat
+++ bin/minify.bat
@@ -1,1 +1,13 @@
-uglifyjs ..\dist\js\Utils.js ..\dist\js\py.js ..\dist\js\Router.js ..\dist\js\Adhara.js ..\dist\js\View.js ..\dist\js\ContainerView.js ..\dist\js\RestAPI.js -o ..\dist\adhara.min.js
+uglifyjs ..\dist\js\Utils.js
+         ..\dist\js\py.js
+         ..\dist\js\Router.js
+         ..\dist\js\Adhara.js
+         ..\dist\js\template.js
+         ..\dist\js\view\View.js
+         ..\dist\js\view\ContainerView.js
+         ..\dist\js\view\SubView.js
+         ..\dist\js\RestAPI.js
+         ..\dist\js\view\TableView.js
+         ..\dist\js\view\Popup.js
+
+         -o ..\dist\adhara.min.js
---
+++ bin/minify.sh
@@ -0,0 +1,13 @@
+uglifyjs ../dist/js/Utils.js
+         ../dist/js/py.js
+         ../dist/js/Router.js
+         ../dist/js/Adhara.js
+         ../dist/js/template.js
+         ../dist/js/view/View.js
+         ../dist/js/view/ContainerView.js
+         ../dist/js/view/SubView.js
+         ../dist/js/RestAPI.js
+         ../dist/js/view/TableView.js
+         ../dist/js/view/Popup.js
+
+         -o ../dist/adhara.min.js
