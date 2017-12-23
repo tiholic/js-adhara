eec1f43437f4a6b8a7dc9215af9d64886127625f
Varun2604 <iyervarun2604@gmail.com>
2017-12-23 18:25:06 +0530

    pushing in correct table and dialog view

--- js/view/TableView.js
+++ js/view/TableView.js
@@ -5,69 +5,26 @@
 
 class AdharaTableView extends AdharaView{
 
-    /***
-     * @param url The api URL
-     * @config the configurations required for the table
-     *          the config object comprises of the following:
-     *          1. columns - array - the colummns for the table. (Only these fields are fetched by the url)
-     *          2. url - string - the apiurl
-     *          2. title - string - the title for the table
-     *          3. add_new - boolean - defaulted to false, "add new" button will be present if set as true
-     *          4. nav - boolean - defaulted to false, will display navigations if set as true
-     */
+    constructor(){
+        super();
+    }
 
-    constructor(url, config){
-        super('table');
-        this.url = url;
-        this.config = config;
-        AdharaTableView.validateConf(this.config);
+    get config(){
+        return {
+            title : 'Adhara Table',
+            add_new : false,
+            nav : false
+        }
+    }
+
+    get template(){
+        return 'table'
     }
 
     render(containerSelector){
-        this.initFetch(containerSelector);
-    }
-
-    initFetch(containerSelector){
-        jQuery.ajax({
-            url : this.config.url,
-            method : 'GET',
-            success : function(data){
-                this.data = data;
-                this.drawTable(containerSelector);
-            },
-            error : function(){
-
-            }
-        });
-    }
-
-    drawTable(containerSelector){
         this.container = document.querySelector(containerSelector);
         this.container.innerHTML = Handlebars.templates[this.template]({'config' : this.config, 'data' : this.data});
-        this.format(this.container);
+        this.postProcess(this.container);
     }
 
-    static validateConf(conf){
-        let errors = [];
-        if(!conf.url){
-            errors.push("No URL in config.")
-        }
-        if(!conf.columns){
-            errors.push("No COLUMNS in config.")
-        }
-        if(!conf.title){
-            errors.push("No TITLE in config.")
-        }
-        if(conf.add_new == undefined){
-            conf.add_new = true;
-        }
-        if(conf.nav == undefined){
-            conf.nav = false;
-        }
-
-        if(!errors.length == 0){
-            throw new Error(errors.join(','));
-        }
-
-    }
 }
