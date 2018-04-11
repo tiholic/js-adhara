98c3d26a75353793a617a489b1ef45d159b58d5e
Rohit Reddy Abbadi <rr.16566@gmail.com>
2018-04-11 19:36:05 +0530

    controller doc strings added. moved Controllers.control global method inside controller

--- js/DataInterface/Controller.js
+++ js/DataInterface/Controller.js
@@ -1,27 +1,76 @@
 class AdharaController{
 
-    control(method, context, data){
-        Controller.control(method, context, data);
+    /**
+     * @function
+     * @instance
+     * @param {String} method - can be any of [get|get_list|put|post|delete]
+     * @param {Object} entity_config - entity configuration
+     * @param {*} data - data to be passed as a payload/query params
+     * @param {Object} options - options for DI handling! might not be necessary
+     * @description initial hook to handle a data call
+     * */
+    control(method, entity_config, data, options){
+        return Adhara.dataInterface.enqueue(method, entity_config, data, options);
+    }
+
+    /**
+     * @function
+     * @instance
+     * @param {Object} entity_config - entity configuration
+     * @param {*} data - data to be passed as a payload/query params
+     * @param {Object} options - options for DI handling! might not be necessary
+     * @description initial hook to handle a get data call
+     * */
+    getData(entity_config, data, options){
+        this.control('get', entity_config, data, options);
     };
 
-    getData(context, data){
-        this.control('get', context, data);
+    /**
+     * @function
+     * @instance
+     * @param {Object} entity_config - entity configuration
+     * @param {*} data - data to be passed as a payload/query params
+     * @param {Object} options - options for DI handling! might not be necessary
+     * @description initial hook to handle a get list data call
+     * */
+    getListData(entity_config, data, options){
+        this.control('get_list', entity_config, data, options);
     };
 
-    getListData(context, data){
-        this.control('get_list', context, data);
+    /**
+     * @function
+     * @instance
+     * @param {Object} entity_config - entity configuration
+     * @param {*} data - data to be passed as a payload/query params
+     * @param {Object} options - options for DI handling! might not be necessary
+     * @description initial hook to handle a put data call
+     * */
+    putData(entity_config, data, options){
+        this.control('put', entity_config, data, options);
     };
 
-    putData(context, data){
-        this.control('put', context, data);
+    /**
+     * @function
+     * @instance
+     * @param {Object} entity_config - entity configuration
+     * @param {*} data - data to be passed as a payload/query params
+     * @param {Object} options - options for DI handling! might not be necessary
+     * @description initial hook to handle a post data call
+     * */
+    postData(entity_config, data, options){
+        this.control('post', entity_config, data, options);
     };
 
-    postData(context, data){
-        this.control('post', context, data);
-    };
-
-    deleteData(context, data){
-        this.control('delete', context, data);
+    /**
+     * @function
+     * @instance
+     * @param {Object} entity_config - entity configuration
+     * @param {*} data - data to be passed as a payload/query params
+     * @param {Object} options - options for DI handling! might not be necessary
+     * @description initial hook to handle a delete data call
+     * */
+    deleteData(entity_config, data, options){
+        this.control('delete', entity_config, data, options);
     };
 
 }
