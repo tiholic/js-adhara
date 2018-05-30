fe2738c0c363d489d72359d31ea3441f3fd63968
Rohit Reddy Abbadi <rr.16566@gmail.com>
2018-05-30 10:19:59 +0530

    HandlebarUtils changed to TemplateUtils. also Adhara.app.renderTemplate is introduced. Override this method in extended App for custom renderer

--- js/view/View.js
+++ js/view/View.js
@@ -342,7 +342,7 @@ class AdharaView extends AdharaController{
     }
 
     _getHTML(template){
-        return TemplateUtils.execute(
+        return Adhara.app.renderTemplate(
             template || this.getTemplate(),
             this
         );
